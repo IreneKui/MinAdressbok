68d1c28af1be79f070a9e7830d261a3ba30ce1f0
Irene Ku Iveling <82irekui@skola.boras.se>
2020-11-10 09:27:27 +0100

    generated list

--- Domain/Adressbook.cs
+++ Domain/Adressbook.cs
@@ -2,4 +2,19 @@ using System.Collections.Generic;
 public class AddressBook
 {
     private List<Contact> contactList = new List<Contact>();
+
+    public void Add(Contact contact)
+    {
+        contactList.Add(contact);
+    }
+
+    public void AddContacts(List<Contact> contacts)
+    {
+        contactList.AddRange(contacts);
+    }
+
+    public List<Contact> GetContacts()
+    {
+        return new List<Contact>(contactList);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Addressbok
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Skapa adressbok
+            AddressBook addressBook = new AddressBook();
         }
     }
 }
