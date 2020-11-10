bfec87cb368a32c5d1064e0ebd44b17434058757
Irene Ku Iveling <82irekui@skola.boras.se>
2020-11-10 09:35:19 +0100

    generated fake contacts

--- Program.cs
+++ Program.cs
@@ -8,6 +8,27 @@ namespace Addressbok
         {
             //Skapa adressbok
             AddressBook addressBook = new AddressBook();
+
+            //Lägg in några kontakter från början
+            GenerateFakecontacts(addressBook);
+        }
+
+        static void GenerateFakecontacts(AddressBook addressBook)
+        {
+            //1. Lägg till kontakt via UI så småningom
+            BusinessContact nyKontakt;
+            nyKontakt = new BusinessContact();
+            nyKontakt.FirstName = "Lisa";
+            nyKontakt.Company ="BY";
+
+            addressBook.Add(nyKontakt);
+
+            PrivateContact nyPrivatKontakt = new PrivateContact();
+            nyPrivatKontakt.FirstName = "Arne";
+            nyPrivatKontakt.RelationType ="It's compicated";
+            nyPrivatKontakt.Birthday = new DateTime(1981, 6 , 1);
+
+            addressBook.Add(nyPrivatKontakt);
         }
     }
 }
